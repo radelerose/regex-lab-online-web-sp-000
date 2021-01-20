b2954e743d9b65baf45c6a2a95c8fbd8d4fc8dc0
Radele Rose <radele.rose@gmail.com>
2021-01-20 20:16:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,11 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+  word.match(\b\w{5}\b)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  word.match(\A[A-Z].+\W\z)
 end
 
 def valid_phone_number?(phone)
